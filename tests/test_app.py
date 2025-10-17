8ba0f0d5c54c5ef135e54515ec087a80e9d15e0e
irina <llininirina@gmail.com>
2025-10-16 21:26:21 -0500

    Fix flake8 formatting issues

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,9 @@
-
-
-import sys
 import os
-
-# Asegura que el directorio raíz esté en el path para poder importar app.py
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
+import sys
 from app import reverse_text
 
 
+# --- Tests ---
 def test_reverse_basic():
     assert reverse_text("hola") == "aloh"
 
