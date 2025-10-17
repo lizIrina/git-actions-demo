4f26b509fadd342b5323276f080e84b939de05f9
irina <llininirina@gmail.com>
2025-10-16 21:20:31 -0500

    Fix CI imports and tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,21 @@
+
+
 import sys
 import os
+
+# Asegura que el directorio raíz esté en el path para poder importar app.py
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from app import reverse_text
 
-from app import reverse_text
 
 def test_reverse_basic():
     assert reverse_text("hola") == "aloh"
 
+
 def test_reverse_empty():
     assert reverse_text("") == ""
 
+
 def test_reverse_palindrome():
-    assert reverse_text("liz") == "liz"
+    assert reverse_text("ana") == "ana"
